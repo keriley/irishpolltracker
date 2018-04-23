f75c79cd6043ca1566dee5baf2b61ca24e87e885
keriley <kteriley@gmail.com>
2018-04-23 18:25:37 +0100

    Update data-table.js

--- js/data-table.js
+++ js/data-table.js
@@ -1,14 +1,19 @@
+//calls google visualisation library table
 google.load('visualization', '1', {
     packages: ['table']
 });
+
+//defines empty variable for the table
 var visualization;
 
+//queries public spreadsheet to extract data and label columns, triggers handleQueryResponse function
 function drawVisualization() {
     var query = new google.visualization.Query('https://spreadsheets.google.com/tq?key=1ySIfax8AqawiXRl6pFkVx0-NXOiDgO_-NzdBbLVFtBE&output=html&gid=0&usp=sharing');
     query.setQuery('SELECT A, B, D, E, F, G, H, I label A "DATE", B "POLLSTER", D "Fine Gael", E "Fianna F&aacute;il", F "Labour", G "Sinn F&eacute;in", H "Green Party", I "Other/Ind."');
     query.send(handleQueryResponse);
 }
 
+//takes data extracted from public spreadsheet, draws table and binds it to html element
 function handleQueryResponse(response) {
     if (response.isError()) {
         alert('There was a problem with your query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
@@ -21,4 +26,6 @@ function handleQueryResponse(response) {
         legend: 'bottom'
     });
 }
+
+//ready, set, go!
 google.setOnLoadCallback(drawVisualization);
